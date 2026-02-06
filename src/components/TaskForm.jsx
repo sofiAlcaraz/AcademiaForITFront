1f675127961853ad74da98d15de32c0a28e5b116
sofiAlcaraz <sofiialcaraz16@gmail.com>
2026-02-06 14:11:36 -0300

    Se agrego formulario para crear y editar tareas

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,79 @@
+import { Box, Button, Modal, TextField, Typography } from "@mui/material";
+import StatusFormSelect from "./StatusFormSelect";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 900,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+function TaskForm({
+  modalForm,
+  setModalForm,
+  handleChange,
+  handleSubmit,
+  task,
+}) {
+  return (
+    <Modal
+      open={true}
+      onClose={() => setModalForm({ open: false, mode: null })}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography
+          color="black"
+          id="modal-modal-title"
+          variant="h4"
+          component="h2"
+        >
+          {modalForm.mode === "create" ? "Crear Tarea" : "Editar tarea"}
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{
+            width: 300,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            m: "auto",
+            mt: 4,
+          }}
+        >
+          <TextField
+            label="Título"
+            name="title"
+            defaultValue={task.title}
+            required
+            onChange={handleChange}
+          />
+
+          <TextField
+            label="Descripción"
+            name="description"
+            defaultValue={task.decription}
+            required
+            multiline
+            maxRows={10}
+            minRows={10}
+            onChange={handleChange}
+          />
+          <StatusFormSelect handleChange={handleChange} status={task.status} />
+
+          <Button type="submit" variant="contained">
+            Guardar
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  );
+}
+export default TaskForm;
