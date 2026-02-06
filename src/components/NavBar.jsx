96cb0cf97c4f4490ca9f6b9789e369f691832015
sofiAlcaraz <sofiialcaraz16@gmail.com>
2026-02-06 14:12:50 -0300

    Se agrego navBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,51 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+
+function NavBar({ setModalForm }) {
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#110a26" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: "flex",
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Mis tareas
+          </Typography>
+
+          <Box
+            sx={{ display: "flex", flexGrow: 1, justifyContent: "flex-end" }}
+          >
+            <Button
+              onClick={() =>
+                setModalForm({
+                  open: true,
+                  mode: "create",
+                })
+              }
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              Crear tarea
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default NavBar;
